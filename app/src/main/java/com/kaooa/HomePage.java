d8e72ca31d2637766d076efaa18864f53c1b31be
Yashas Ip <67045923+yashasip@users.noreply.github.com>
2022-05-02 11:50:01 +0530

    Kaooa Project Initialized

---
+++ app/src/main/java/com/kaooa/HomePage.java
@@ -0,0 +1,14 @@
+package com.kaooa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_home);
+    }
+}
