d19b2e1fadddc984d4192c2dda5198699d24bffe
Yashas Ip <67045923+yashasip@users.noreply.github.com>
2022-05-06 22:32:45 +0530

    Added GamePage and Switch to GamePage
    - added new game page activity
    - applied theme to game page
    - removed xml onClick Listener of Tap to Play
    - added a onClick Interface to Tap to Play which switches to Game Page

---
+++ app/src/main/java/com/kaooa/GamePage.java
@@ -0,0 +1,14 @@
+package com.kaooa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GamePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_page);
+    }
+}
