106ed47e2180b50cc7b74d01efc2445682192a76
Yashas Ip <67045923+yashasip@users.noreply.github.com>
2022-05-21 21:54:03 +0530

    GamePage Structure changes
    - added PointSpaces of Star Map
    - renamed .xml of GamePage
    - changed app padding

--- app/src/main/java/com/kaooa/GamePage.java
+++ app/src/main/java/com/kaooa/GamePage.java
@@ -9,6 +9,6 @@ public class GamePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_page);
+        setContentView(R.layout.game_page);
     }
 }
